b8d4304c3465637894ef0854f4a4ff686591a7fc
Jhuly Ambrozini <jhulyambrozini@gmail.com>
2024-07-27 21:59:14 -0300

    feat: rota de criar viagem

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import fastify from "fastify";
+import { createTrip } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import cors from '@fastify/cors'
 
 const app = fastify()
+
+app.register(cors, ({
+    origin: 'http://localhost:3000'
+}))
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createTrip)
+app.register(confirmTrip)
+
 
 app.listen({port: 3333}).then(() => console.log('Server is running!'))
